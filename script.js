631c90bf8e8e3dfbb23703e257d0253629d34c31
Saskia Opdam <opdamsaskia@icloud.com>
2021-11-26 20:21:09 +0100

    slide menu

---
+++ script.js
@@ -0,0 +1,9 @@
+const showMenu = () => {
+  const slide = document.getElementById("slide-menu");
+  slide.classList.add("show");
+};
+
+const closeMenu = () => {
+  const slide = document.getElementById("slide-menu");
+  slide.classList.remove("show");
+};
