6d420e08d3f0b487221be1c5f335b38c7141f3b1
Saskia Opdam <opdamsaskia@icloud.com>
2021-11-27 20:05:09 +0100

    navigation done

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,17 @@
 const showMenu = () => {
   const slide = document.getElementById("slide-menu");
-  slide.classList.add("show");
+  slide.classList.add("slide-menu-open");
 };
 
 const closeMenu = () => {
   const slide = document.getElementById("slide-menu");
-  slide.classList.remove("show");
+  slide.classList.remove("slide-menu-open");
+};
+
+const toggleSearchfield = () => {
+  const navbarBottom = document.getElementById("navbar-bottom");
+  navbarBottom.classList.toggle("navbar-bottom-displayed");
+
+  const main = document.getElementById("main");
+  main.classList.toggle("main-bigger-margin");
 };
